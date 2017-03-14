a2e511184ba52a40c6a62854163a2da24aa19e04
Abraham Hosch <hosch250@live.com>
2017-03-14 15:59:51 -0500

    Revert "Kill the routing"
    This reverts commit d09979481d3ad9fbd282de733af3ebe313b27f0c.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
  */
 
 var express = require('express')
+  , routes = require('./routes')
   , http = require('http')
   , path = require('path');
 
@@ -23,6 +24,8 @@ app.configure(function(){
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
+
+app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
