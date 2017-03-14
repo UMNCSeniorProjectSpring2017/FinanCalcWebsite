e5bb3fa9c13d2a63c7955af6d1250a828fe5cadd
Abraham Hosch <hosch250@live.com>
2017-03-14 15:45:07 -0500

    Add a few pages

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
+  , calculatorRoutes = require('./routes/calculator')
   , http = require('http')
   , path = require('path');
 
@@ -14,9 +14,6 @@ var app = express();
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -28,7 +25,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/FutureValueCalculator', routes.calculator);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
