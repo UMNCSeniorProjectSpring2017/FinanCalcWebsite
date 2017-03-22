07e88c96fb010fd00678d05866f151917cac842f
singhmg <mike_singh1@yahoo.com>
2017-03-21 20:45:35 -0500

    New ROE Calc

---
+++ public/javascripts/ROECalculator.js
@@ -0,0 +1,43 @@
+﻿"use strict";
+
+function isFloat(str) {
+    for (var i in str) {
+        var chr = str[i]
+        if (isNaN(parseInt(chr)) && chr != '.') {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function $calculate() {
+    if (!isFloat($("#NetEarnings")[0].value) ||
+        !isFloat($("#StockHoldersEquity")[0].value) || 
+        !parseFloat($("#StockHoldersEquity")[0].value)) {
+
+        $("#result").html("Bad argument(s)");
+        return;
+    }
+
+    var netearnings = parseFloat($("#NetEarnings")[0].value);
+    var stockholdersequity = parseFloat($("#StockHoldersEquity")[0].value);
+  
+    var ReturnOnEquity = NetEarnings/StockholdersEquity;
+    console.log(ReturnOnEquity);
+    $("#result").html(ReturnOnEquity);
+    
+    recordUsage()
+}
+
+function recordUsage() {
+    $.post("/LogUsage", { calculator: "ROECalculator" });
+}
+
+function $isValidFloat(id) {
+    if (!isFloat($(id)[0].value)) {
+        $(id).css("color", "Red");
+    } else {
+        $(id).css("color", "Black");
+    }
+}
