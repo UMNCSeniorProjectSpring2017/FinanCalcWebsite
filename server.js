a7ba4d716ab747a243d0efa24c91669a3a62f2c9
Abraham Hosch <hosch250@live.com>
2017-04-16 21:15:29 -0500

    Try to find the problem by trial and error

--- server.js
+++ server.js
@@ -48,9 +48,9 @@ app.get('/AverageCollectionPeriodCalculator', calculator.averageCollectionPeriod
 app.get('/EffectiveInterestRateCalculator', calculator.effectiveInterestRateCalculator);
 app.get('/CostOfMissingDiscountCalculator', calculator.costOfMissingDiscountCalculator);
 app.get('/EconomicOrderingQuantityCalculator', calculator.EOQCalculator);
-/*app.get('/PaybackPeriodCalculator', calculator.paybackPeriodCalculator);
+app.get('/PaybackPeriodCalculator', calculator.paybackPeriodCalculator);
 app.get('/EarningsPerShareCalculator', calculator.earningsPerShareCalculator);
-app.get('/GrossProfitMarginCalculator', calculator.grossProfitMarginCalculator);
+/*app.get('/GrossProfitMarginCalculator', calculator.grossProfitMarginCalculator);
 app.get('/OperatingProfitCalculator', calculator.operatingProfitCalculator);
 app.get('/ReturnOnAssetsCalculator', calculator.returnOnAssetsCalculator);
 app.get('/RequiredRateOfReturnCalculator', calculator.requiredRateOfReturnCalculator);*/
