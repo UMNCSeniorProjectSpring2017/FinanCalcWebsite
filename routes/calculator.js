facb2755c19697facf3074e180f1647ef286824b
bema003 <bema003hl@yahoo.com>
2017-04-19 18:06:36 -0500

    fixing problem with routing with 2 calculators

--- routes/calculator.js
+++ routes/calculator.js
@@ -140,9 +140,9 @@ exports.riskFreeRateOfReturnCalculator = function (req, res) {
 };
 
 exports.presentValueOfPrincipalPaymentAtMaturityCalculator = function (req, res) {
-    res.render('PresentValueOfPrincipalPaymentAtMaturityCalculator', { title: 'ejs' });
+    res.render('Calculators/PresentValueOfPrincipalPaymentAtMaturityCalculator', { title: 'ejs' });
 };
 
 exports.presentValueOfABondsInterestPaymentsCalculator = function (req, res) {
-    res.render('PresentValueOfABondsInterestPaymentsCalculator', { title: 'ejs' });
+    res.render('Calculators/PresentValueOfABondsInterestPaymentsCalculator', { title: 'ejs' });
 };
