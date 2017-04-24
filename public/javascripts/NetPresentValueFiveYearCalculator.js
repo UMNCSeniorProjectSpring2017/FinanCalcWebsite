d33f08acc05ba547babfc3243bd99bcc52bd091c
Hosch250 <Hosch250@users.noreply.github.com>
2017-04-23 22:23:45 -0500

    Fix indentation

--- public/javascripts/NetPresentValueFiveYearCalculator.js
+++ public/javascripts/NetPresentValueFiveYearCalculator.js
@@ -17,29 +17,25 @@ function $calculate() {
 	
     var initialIvestment = parseFloat($("#initialIvestment")[0].value);
     var discountRate = parseFloat($("#discountRate")[0].value);
-        var adjDiscountRate = (discountRate * .01) +1;
+    var adjDiscountRate = (discountRate * .01) +1;
 	   
     var firstYear = parseFloat($("#firstYear")[0].value);
-        var firstYearValue = firstYear / adjDiscountRate;
+    var firstYearValue = firstYear / adjDiscountRate;
 	
     var secondYear = parseFloat($("#secondYear")[0].value);
-        var secondYearValue = (secondYear / Math.pow(adjDiscountRate, 2));
+    var secondYearValue = (secondYear / Math.pow(adjDiscountRate, 2));
 	                
     var thirdYear = parseFloat($("#thirdYear")[0].value);
-        var thirdYearValue = (thirdYear / Math.pow(adjDiscountRate, 3));
+    var thirdYearValue = (thirdYear / Math.pow(adjDiscountRate, 3));
 	
     var fourthYear = parseFloat($("#fourthYear")[0].value);
-        var fourthYearValue = (fourthYear / Math.pow(adjDiscountRate, 4));
+    var fourthYearValue = (fourthYear / Math.pow(adjDiscountRate, 4));
 	
     var fifthYear = parseFloat($("#fifthYear")[0].value);
-        var fifthYearValue = (fifthYear / Math.pow(adjDiscountRate, 5));
+    var fifthYearValue = (fifthYear / Math.pow(adjDiscountRate, 5));
 
     var netPresentValue = firstYearValue + secondYearValue + thirdYearValue +  fourthYearValue + fifthYearValue - initialIvestment;
-    
-	
     $("#result").html(netPresentValue);
  
-   
-
     recordUsage("NetPresentValueFievYearCalculator");
 }
